7e57df36c51dcd0ac64746007bf9506b10bb47ee
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2022-01-20 14:42:22 +0200

    Entities' configs

---
+++ ITCompany/ITCompany/EntitiesConfigs/EmployeeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using ITCompany.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ITCompany.EntitiesConfigs
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<EmployeeEntity>
+    {
+        public void Configure(EntityTypeBuilder<EmployeeEntity> builder)
+        {
+            builder.ToTable("Employee").HasKey(e => e.EmployeeId);
+            builder.Property(e => e.EmployeeId).ValueGeneratedOnAdd();
+            builder.Property(e => e.FirstName).IsRequired().HasMaxLength(50);
+            builder.Property(e => e.LastName).IsRequired().HasMaxLength(50);
+            builder.Property(e => e.HiredDate).IsRequired().HasColumnType("datetime2").HasMaxLength(7);
+            builder.Property(e => e.DateOfBirth).HasColumnType("date");
+
+            builder.HasOne(e => e.Office)
+                .WithMany(o => o.Employees)
+                .HasForeignKey(e => e.OfficeId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasOne(e => e.Title)
+                .WithMany(t => t.Employees)
+                .HasForeignKey(e => e.TitleId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
---
+++ ITCompany/ITCompany/EntitiesConfigs/ProjectConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ITCompany.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ITCompany.EntitiesConfigs
+{
+    public class ProjectConfiguration : IEntityTypeConfiguration<ProjectEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProjectEntity> builder)
+        {
+            builder.ToTable("Project").HasKey(p => p.ProjectId);
+            builder.Property(p => p.ProjectId).ValueGeneratedOnAdd();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+            builder.Property(p => p.Budget).IsRequired().HasColumnType("money");
+            builder.Property(p => p.StartedDate).IsRequired().HasColumnType("datetime2").HasMaxLength(7);
+        }
+    }
+}
