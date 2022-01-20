79f0e81e32f902c810319c3de7c1f44fb4370693
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2022-01-20 14:43:56 +0200

    config file + db init in program.cs

--- ITCompany/ITCompany/Program.cs
+++ ITCompany/ITCompany/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using System.IO;
+using System.Threading.Tasks;
+using ITCompany.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ITCompany
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string configPath = "appsettings.json";
+            var builder = new ConfigurationBuilder();
+            var currentDirectory = Directory.GetCurrentDirectory();
+            builder.SetBasePath(currentDirectory);
+            builder.AddJsonFile(configPath);
+            var config = builder.Build();
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionBuilder = new DbContextOptionsBuilder<ITCompanyDbContext>();
+            var options = optionBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+
+            using (var db = new ITCompanyDbContext(options))
+            {
+                await db.SaveChangesAsync();
+            }
         }
     }
 }
