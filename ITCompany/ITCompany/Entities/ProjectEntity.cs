1b61b97c0f8a5af2654072f48787561993f85009
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2022-01-20 14:41:52 +0200

    Entities

---
+++ ITCompany/ITCompany/Entities/ProjectEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ITCompany.Entities
+{
+    public class ProjectEntity
+    {
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+        public decimal Budget { get; set; }
+        public DateTime StartedDate { get; set; }
+        public List<EmployeeEntity> Employees { get; set; } = new List<EmployeeEntity>();
+        public List<EmployeeProjectEntity> EmployeeProject { get; set; } = new List<EmployeeProjectEntity>();
+    }
+}
