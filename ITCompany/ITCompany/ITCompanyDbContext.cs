0567903d25031e46ec37ed80eb477c4eea781591
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2022-01-20 14:42:36 +0200

    Context

---
+++ ITCompany/ITCompany/ITCompanyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using ITCompany.Entities;
+using ITCompany.EntitiesConfigs;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITCompany
+{
+    public class ITCompanyDbContext : DbContext
+    {
+        public ITCompanyDbContext(DbContextOptions<ITCompanyDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<EmployeeEntity> Employees { get; set; }
+        public DbSet<TitleEntity> Titles { get; set; }
+        public DbSet<ProjectEntity> Projects { get; set; }
+        public DbSet<OfficeEntity> Offices { get; set; }
+        public DbSet<EmployeeProjectEntity> EmployeeProjects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new TitleConfiguration());
+            modelBuilder.ApplyConfiguration(new ProjectConfiguration());
+            modelBuilder.ApplyConfiguration(new OfficeConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeProjectConfiguration());
+        }
+    }
+}
